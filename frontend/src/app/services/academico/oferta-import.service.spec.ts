a9d984431f418e8d40959a0ae1f8d6b74f1ea057
JoseAB32 <arias.balderrama.josmearia@gmail.com>
2026-05-09 17:22:15 -0400

    feta: se agrego test a oferta-import,service para pasar el pipeline

--- frontend/src/app/services/academico/oferta-import.service.spec.ts
+++ frontend/src/app/services/academico/oferta-import.service.spec.ts
@@ -1,16 +1,69 @@
 import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
-import { OfertaImportService } from './oferta-import.service';
+import { ApiService } from '../api.service';
+import {
+  OfertaImportResultResponse,
+  OfertaImportService,
+} from './oferta-import.service';
 
 describe('OfertaImportService', () => {
   let service: OfertaImportService;
+  let apiServiceSpy: jasmine.SpyObj<ApiService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    apiServiceSpy = jasmine.createSpyObj<ApiService>('ApiService', ['post']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        OfertaImportService,
+        {
+          provide: ApiService,
+          useValue: apiServiceSpy,
+        },
+      ],
+    });
+
     service = TestBed.inject(OfertaImportService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should send the selected CSV file to the academic offers import endpoint', (done) => {
+    const file = new File(['contenido'], 'ofertas.csv', {
+      type: 'text/csv',
+    });
+
+    const response: OfertaImportResultResponse = {
+      summary: {
+        totalRows: 1,
+        offersCreated: 1,
+        offersUpdated: 0,
+        scheduleBlocks: 1,
+        skippedRows: 0,
+        errorsCount: 0,
+        warningsCount: 0,
+      },
+      offers: [],
+      errors: [],
+      warnings: [],
+    };
+
+    apiServiceSpy.post.and.returnValue(of(response));
+
+    service.importarOfertas(1, file).subscribe((result) => {
+      expect(result).toEqual(response);
+      expect(apiServiceSpy.post).toHaveBeenCalledTimes(1);
+
+      const [url, body] = apiServiceSpy.post.calls.mostRecent().args;
+
+      expect(url).toBe('/api/academico/ofertas/importar?mallaId=1');
+      expect(body instanceof FormData).toBeTrue();
+      expect((body as FormData).get('file')).toBe(file);
+
+      done();
+    });
   });
 });
